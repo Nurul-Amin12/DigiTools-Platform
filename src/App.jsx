15b850078bcde74989fb010bd7534613adf61cfc
nurul <nurulamin31219@gmail.com>
2026-04-10 01:37:04 +0600

    Navbar Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+
 
 function App() {
   
 
   return (
     <>
-      <h2 className='text-5xl text-red-500 p-5 bg-amber-100 mx-auto w-60 font-bold'>Bismillah</h2>
+      <div>
+        <Navbar/>
+      </div>
     </>
   )
 }
