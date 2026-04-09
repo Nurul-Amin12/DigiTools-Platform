187b9ff1dd9d536f59b9270b81b5e41d54d29c8e
nurul <nurulamin31219@gmail.com>
2026-04-09 21:32:23 +0600

    Initialize Project with all resourses & invironment setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h2 className='text-5xl text-red-500 p-5 bg-amber-100 mx-auto w-60 font-bold'>Bismillah</h2>
+    </>
+  )
+}
+
+export default App
