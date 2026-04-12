ddc76524dc21850532deca7f43a56d011cf3b220
nurul <nurulamin31219@gmail.com>
2026-04-12 15:47:46 +0600

    Model Design Work

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,18 @@ import Banner from './Components/Banner/Banner'
 import Summary from './Components/Banner/Summary'
 import Footer from './Components/Footer/Footer'
 import GEtStarted from './Components/GetStart/GetStarted'
+import Model from './Components/Models/Model'
 import Navbar from './Components/Navbar/Navbar'
 import Pricing from './Components/Pricing/Pricing'
 import Transform from './Components/Pricing/Transform'
+
+
+const getModels = async() => {
+    const res = await fetch('/models.json')
+    return res.json();
+}
+
+const modelPromise = getModels();
 
 
 function App() {
@@ -22,6 +31,10 @@ function App() {
         <section>
           <Banner/>
           <Summary/>
+        </section>
+
+        <section>
+          <Model modelPromise={modelPromise}/>
         </section>
 
         <section>
